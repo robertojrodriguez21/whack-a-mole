51d6113512a3f1f2a960826aebe1f26ec996719b
Roberto J Rodriguez <robertojrodriguez21@gmail.com>
2022-10-03 14:16:48 -0500

    Got page switch buttons to work

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
+// Page Select
 const playGameButton = document.getElementById(`playGameButton`)
 const homeButton = document.getElementById(`homeButton`)
 
-playGameButton.addEventListener(`click`, () => {
-  document.location.href = `game.html`
-})
+if (playGameButton) {
+  playGameButton.addEventListener(`click`, () => {
+    window.location.href = `game.html`
+    console.log(`click`)
+  })
+}
 
-homeButton.addEventListener(`click`, () => {
-  document.location.href = `index.html`
-})
+if (homeButton) {
+  homeButton.addEventListener(`click`, () => {
+    window.location.href = `index.html`
+    console.log(`click`)
+  })
+}
+
+// Game Logic
+// Variables
+let gameOver = false
+
+while (!gameOver) {}
