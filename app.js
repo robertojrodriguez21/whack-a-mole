4a2153118ccaf264aa0433ce41ab788f9314f3be
Roberto J Rodriguez <robertojrodriguez21@gmail.com>
2022-10-03 16:03:16 -0500

    Added basic mole pop up

--- app.js
+++ app.js
@@ -16,8 +16,44 @@ if (homeButton) {
   })
 }
 
-// Game Logic
+// Game
 // Variables
-let gameOver = false
+let gameTimer = 10
+let randomIndex = 0
+const gameboardBoxesValues = [
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+]
 
-while (!gameOver) {}
+const gameboardBoxes = document.querySelectorAll(`.gameboardBox`)
+const startGameButton = document.getElementById(`startGameButton`)
+
+// Functions
+const startGame = () => {
+  for (let i = 0; i < gameboardBoxes.length; i++) {
+    gameboardBoxes[i].addEventListener(`click`, () => {
+      if (gameboardBoxes[i] === 1) {
+        gameboardBoxes[i].style.backgroundColor = `black`
+      }
+    })
+  }
+
+  while (gameTimer !== 0) {
+    randomIndex = Math.floor(Math.random() * 25)
+    while (gameboardBoxes[randomIndex] === 1) {
+      randomIndex = Math.floor(Math.random() * 25)
+    }
+    gameboardBoxesValues[randomIndex] = 1
+    gameboardBoxes[randomIndex].style.backgroundColor = `yellow`
+    setInterval(2000)
+    gameTimer--
+  }
+}
+
+// Logic
+startGameButton.addEventListener('click', startGame)
+
+for (let i = 0; i < gameboardBoxes.length; i++) {
+  gameboardBoxes[i].addEventListener(`click`, () => {
+    gameboardBoxes[i].style.backgroundColor = `yellow`
+  })
+}
