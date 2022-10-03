65b6703294f919aeebae1218429be04d7d9d7902
Roberto J Rodriguez <robertojrodriguez21@gmail.com>
2022-10-03 13:15:11 -0500

    Created grid for game on game page

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-const playGameButton = document.getElementById(`landingButton`)
+const playGameButton = document.getElementById(`playGameButton`)
+const homeButton = document.getElementById(`homeButton`)
+
 playGameButton.addEventListener(`click`, () => {
   document.location.href = `game.html`
+})
+
+homeButton.addEventListener(`click`, () => {
+  document.location.href = `index.html`
 })
